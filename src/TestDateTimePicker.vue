e0cca69b6faf6bdefeaf5625ea929b9a20d67eab
Gabriel Rodrigues <0040892@academico.ifmg.edu.br>
2022-07-28 21:28:06 -0300

    updating not miliseconds type problem

--- src/TestDateTimePicker.vue
+++ src/TestDateTimePicker.vue
@@ -1,16 +1,19 @@
 <template>
-  <v-container>
-    <v-row>
-      <v-col cols="12" lg="4">
-        <v-text-field-datetimepicker
-          v-model="value1"
-          label="Data Hora"
-          v-bind:properties="{
+	<v-container>
+		<v-row>
+			<v-col
+				cols="12"
+				lg="4"
+			>
+				<v-text-field-datetimepicker
+					v-model="value1"
+					label="Data Hora"
+					v-bind:properties="{
             clearable: false,
             outlined: true,
             appendIcon: 'mdi-av-timer'
           }"
-          v-bind:options="{
+					v-bind:options="{
             tabDateTitle: 'Data',
             tabTimeTitle: 'Hora',
             tabBackgroundColor: 'green',
@@ -20,22 +23,25 @@
             closeOnDateClick: false,
             useSeconds: false
           }"
-        />
-        v-model: {{ value1 }} (milliseconds)
-      </v-col>
-      <v-col cols="12" lg="4">
-        <v-text-field-datetimepicker
-          v-model="value2"
-          label="Date Time Picker"
-          v-bind:properties="{
+				/>
+				v-model: {{ value1 }} (milliseconds)
+			</v-col>
+			<v-col
+				cols="12"
+				lg="4"
+			>
+				<v-text-field-datetimepicker
+					v-model="value2"
+					label="Date Time Picker"
+					v-bind:properties="{
             backgroundColor: '#EEE9E9',
             clearable: false,
             outlined: false,
             prependIcon: 'mdi-calendar',
             appendIcon: ''
           }"
-          time
-          v-bind:options="{
+					time
+					v-bind:options="{
             tabDateTitle: 'Date',
             tabTimeTitle: 'Time',
             tabBackgroundColor: 'blue',
@@ -45,21 +51,24 @@
             closeOnDateClick: false,
             useSeconds: false
           }"
-        />
-        v-model: {{ value2 }} (milliseconds)
-      </v-col>
-      <v-col cols="12" lg="4">
-        <v-text-field-datetimepicker
-          v-model="value3"
-          label="Date Time Picker"
-          v-bind:properties="{
+				/>
+				v-model: {{ value2 }} (milliseconds)
+			</v-col>
+			<v-col
+				cols="12"
+				lg="4"
+			>
+				<v-text-field-datetimepicker
+					v-model="value3"
+					label="Date Time Picker"
+					v-bind:properties="{
             backgroundColor: '',
             clearable: false,
             outlined: false,
             prependIcon: '',
             appendIcon: ''
           }"
-          v-bind:options="{
+					v-bind:options="{
             tabDateTitle: 'Date',
             tabTimeTitle: 'Time',
             tabBackgroundColor: 'red',
@@ -69,27 +78,27 @@
             closeOnDateClick: false,
             useSeconds: true
           }"
-        />
-        v-model: {{ value3 }} (milliseconds)
-      </v-col>
-    </v-row>
-  </v-container>
+				/>
+				v-model: {{ value3 }} (milliseconds)
+			</v-col>
+		</v-row>
+	</v-container>
 </template>
 
 <script>
 import DateTimePicker from "@/components/DateTimePicker.vue";
 
 export default {
-  name: "app",
-  components: {
-    "v-text-field-datetimepicker": DateTimePicker
-  },
-  data: () => ({
-    value1: 1558220700000,
+	name: "app",
+	components: {
+		"v-text-field-datetimepicker": DateTimePicker,
+	},
+	data: () => ({
+		value1: "",
 
-    value2: 1558220700000,
+		value2: 1558220700000,
 
-    value3: 1558220710000
-  })
+		value3: 1558220710000,
+	}),
 };
 </script>
